cb1663a7e8e88c7ae281961e121809143f5c6c20
Charlton, Scott R <charlton@usgs.gov>
2021-07-08 19:26:54 -0600

    now debug and release hdf5 are installed to the same directory

--- iriclib-2019.cmake
+++ iriclib-2019.cmake
@@ -7,10 +7,10 @@ set(HDF5_VER "$ENV{HDF5_VER}")
 set(POCO_VER "$ENV{POCO_VER}")
 
 set(CTEST_SOURCE_DIRECTORY "${CTEST_SCRIPT_DIRECTORY}/lib/src/${CTEST_PROJECT_NAME}-git")
-set(CTEST_BINARY_DIRECTORY "lib/build/${CTEST_PROJECT_NAME}-${CTEST_PROJECT_VERSION}")
+set(CTEST_BINARY_DIRECTORY "lib/build/${CTEST_PROJECT_NAME}-${CTEST_PROJECT_VERSION}/${CONF_DIR}")
 
 # build PREFIX_PATH (Note the escaped semicolon)
-set(PREFIX_PATH "${CTEST_SCRIPT_DIRECTORY}/lib/install/hdf5-${HDF5_VER}/${CONF_DIR}/cmake/hdf5")
+set(PREFIX_PATH "${CTEST_SCRIPT_DIRECTORY}/lib/install/hdf5-${HDF5_VER}/cmake/hdf5")
 set(PREFIX_PATH "${PREFIX_PATH}\;${CTEST_SCRIPT_DIRECTORY}/lib/install/poco-${POCO_VER}/lib/cmake/Poco")
 
 # override LIBDIR to be consistent w/ hdf5 and cgns
@@ -25,28 +25,28 @@ CTEST_START("Experimental")
 CTEST_CONFIGURE(BUILD "${CTEST_BINARY_DIRECTORY}"
                 OPTIONS "${BUILD_OPTIONS}")
 CTEST_BUILD(BUILD "${CTEST_BINARY_DIRECTORY}")
-if (WIN32)
-  set(hdf5_dir "${CTEST_SCRIPT_DIRECTORY}/lib/install/hdf5-${HDF5_VER}/${CONF_DIR}")
-  set(poco_dir "${CTEST_SCRIPT_DIRECTORY}/lib/install/poco-${POCO_VER}")
-  set(unit_dir "${CTEST_BINARY_DIRECTORY}/unittests_cgnsfile/${CONF_DIR}")
+# if (WIN32)
+#   set(hdf5_dir "${CTEST_SCRIPT_DIRECTORY}/lib/install/hdf5-${HDF5_VER}/${CONF_DIR}")
+#   set(poco_dir "${CTEST_SCRIPT_DIRECTORY}/lib/install/poco-${POCO_VER}")
+#   set(unit_dir "${CTEST_BINARY_DIRECTORY}/unittests_cgnsfile/${CONF_DIR}")
 
-  if(${CONF_DIR} STREQUAL "debug")
-    # unittests_cgnsfile
-    file(INSTALL "${hdf5_dir}/bin/hdf5_D.dll" "${hdf5_dir}/bin/szip_D.dll" "${hdf5_dir}/bin/zlib_D.dll"
-         DESTINATION ${unit_dir}
-     )
-    file(COPY "${poco_dir}/bin/PocoFoundationd.dll"
-        DESTINATION ${unit_dir}
-    )
-  else()
-    # unittests_cgnsfile
-    file(INSTALL "${hdf5_dir}/bin/hdf5.dll" "${hdf5_dir}/bin/szip.dll" "${hdf5_dir}/bin/zlib.dll"
-         DESTINATION ${unit_dir}
-    )
-    file(COPY "${poco_dir}/bin/PocoFoundation.dll"
-        DESTINATION ${unit_dir}
-    )
-  endif()
-endif()
+#   if(${CONF_DIR} STREQUAL "debug")
+#     # unittests_cgnsfile
+#     file(INSTALL "${hdf5_dir}/bin/hdf5_D.dll" "${hdf5_dir}/bin/szip_D.dll" "${hdf5_dir}/bin/zlib_D.dll"
+#          DESTINATION ${unit_dir}
+#      )
+#     file(COPY "${poco_dir}/bin/PocoFoundationd.dll"
+#         DESTINATION ${unit_dir}
+#     )
+#   else()
+#     # unittests_cgnsfile
+#     file(INSTALL "${hdf5_dir}/bin/hdf5.dll" "${hdf5_dir}/bin/szip.dll" "${hdf5_dir}/bin/zlib.dll"
+#          DESTINATION ${unit_dir}
+#     )
+#     file(COPY "${poco_dir}/bin/PocoFoundation.dll"
+#         DESTINATION ${unit_dir}
+#     )
+#   endif()
+# endif()
 CTEST_BUILD(BUILD "${CTEST_BINARY_DIRECTORY}" TARGET RUN_TESTS)
 CTEST_BUILD(BUILD "${CTEST_BINARY_DIRECTORY}" TARGET install)
